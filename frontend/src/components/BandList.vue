bfbc9d4b80f8c537528e3cb00997a32d14d6b5ce
jjaekkaemi <21600752@handong.edu>
2021-07-28 17:36:37 +0900

    menu에서 search 삭제, bandlist ui 및 기능 구현

--- frontend/src/components/BandList.vue
+++ frontend/src/components/BandList.vue
@@ -1,78 +1,119 @@
 <template>
   <div>
-      <v-list>
-      <v-list-item
-        v-for="chat in recent"
-        :key="chat.title">
-        <v-list-item-icon>
-          <v-icon>mdi-message-outline</v-icon>
-        </v-list-item-icon>
+    <v-data-iterator :items="bands" :search="search" hide-default-footer>
+      <template v-slot:header>
+        <v-toolbar dark color="primary" flat>
+          <v-text-field
+            v-model="search"
+            clearable
+            flat
+            solo-inverted
+            hide-details
+            label="Search"
+            dense
+            append-icon="mdi-magnify"
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+      <template v-slot:default="props">
+        <v-list>
+          <template v-for="(band, index) in props.items">
+            <v-list-item :key="index">
+              <v-list-item-icon class="list-item">
+                <v-icon :color="'green'">mdi-circle-medium</v-icon>
+              </v-list-item-icon>
 
-        <v-list-item-content>
-          <v-list-item-title v-text="chat.title"></v-list-item-title>
-        </v-list-item-content>
+              <v-list-item-content class="list-item left-margin">
+                <v-list-item-title v-text="band.bid"></v-list-item-title>
+              </v-list-item-content>
 
-        <v-list-item-content>
-          <v-list-item-title v-text="chat.title"></v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-icon>
-          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
-            mdi-message-outline
-          </v-icon>
-        </v-list-item-icon>
-        <v-list-item-icon>
-          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
-            mdi-message-outline
-          </v-icon>
-        </v-list-item-icon>
-        <v-list-item-icon>
-          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
-            mdi-message-outline
-          </v-icon>
-        </v-list-item-icon>
-        <v-list-item-icon>
-          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'" @click="log()">
-            mdi-message-outline
-          </v-icon>
-        </v-list-item-icon>
-
-      </v-list-item>
-    </v-list>
+              <v-list-item-content class="list-item">
+                <v-list-item-title v-text="band.name"></v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-icon class="list-item">
+                <v-icon :color="band.active ? 'deep-purple accent-4' : 'grey'">
+                  mdi-watch-variant
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-icon class="list-item">
+                <v-icon :color="band.active ? 'deep-purple accent-4' : 'grey'">
+                  mdi-battery-high
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-icon class="list-item">
+                <v-icon :color="band.active ? 'deep-purple accent-4' : 'grey'">
+                  mdi-signal-cellular-3
+                </v-icon>
+              </v-list-item-icon>
+              <v-list-item-icon class="list-item">
+                <v-btn
+                  depressed
+                  block
+                  class="primary"
+                  @click="log(bands[index])"
+                  >조회</v-btn
+                >
+              </v-list-item-icon>
+            </v-list-item>
+            <v-divider :key="band.bid"></v-divider>
+          </template>
+        </v-list>
+      </template>
+    </v-data-iterator>
   </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      recent: [
-        {
-          active: true,
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Jason',
-        },
-        {
-          active: true,
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Mike',
-        },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Cindy',
-        },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'Ali',
-        },
-      ],
-      previous: [{
-        title: 'Travis',
-        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-      }],
-    }),
-    methods: {
-      log(){
-        console.log("클릭")
-      }
-    }
-  }
+export default {
+  data: () => ({
+    bands: [
+      {
+        active: true,
+        bid: "0x00",
+        name: "Jason",
+        scd: true,
+        battery: 50,
+        siganl: -78,
+      },
+      {
+        active: true,
+        bid: "0x01",
+        name: "Mike",
+        scd: true,
+        battery: 50,
+        siganl: -78,
+      },
+      {
+        active: true,
+        bid: "0x02",
+        name: "Cindy",
+        scd: true,
+        battery: 50,
+        siganl: -78,
+      },
+      {
+        active: false,
+        bid: "0x03",
+        name: "Ali",
+        scd: true,
+        battery: 50,
+        siganl: -78,
+      },
+    ],
+    search: "",
+  }),
+  methods: {
+    log(band) {
+      console.log(band);
+    },
+  },
+};
 </script>
+<style lang="scss">
+.list-item {
+  align-self: center; // [jk]
+}
+.list-item.left-margin {
+  margin-left: -5px;
+}
+</style>
--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -3,14 +3,24 @@
     <v-app-bar color="primary" dark flat class="text-center">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-title class="">{{$router.history.current.name}}</v-toolbar-title>
+      <v-toolbar-title class="">{{
+        $router.history.current.name
+      }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-plus</v-icon>
       </v-btn>
-      <template v-slot:extension v-if="check.search.view">
-        <v-text-field filled outlined dense append-icon="mdi-magnify"></v-text-field>
-      </template>
+      <!-- <template v-slot:extension v-if="check.search.view">
+        <v-text-field
+          dense
+          clearable
+          flat
+          solo-inverted
+          hide-details
+          v-model="searchValue"
+          append-icon="mdi-magnify"
+        ></v-text-field>
+      </template> -->
     </v-app-bar>
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list-item>
@@ -43,43 +53,36 @@
 </template>
 
 <script>
-  export default {
-    name: "Menu",
-    props: {
-      view:{}
-    },
-    data () {
-      return {
-        drawer: null,
-        items: [
-          { title: '조회', icon: 'mdi-watch', to:'/bandlist' },
-          { title: '정보', icon: 'mdi-forum', to:'/bandinfo' },
-        ],
-        check: {
-          nav: {
-            list: ["Login"],
-            view: true
-          },
-          search: {
-            list: ["BAND INFO"],
-            view: true
-          }
+export default {
+  name: "Menu",
+  props: {
+    view: {},
+  },
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "조회", icon: "mdi-watch", to: "/bandlist" },
+        { title: "정보", icon: "mdi-forum", to: "/bandinfo" },
+      ],
+      check: {
+        nav: {
+          list: ["Login"],
+          view: true,
         },
+      },
+      searchValue: null,
+    };
+  },
+  methods: {},
+  watch: {
+    $route(to) {
+      console.log(to.name);
+      if (this.check.nav.list.includes(to.name)) this.check.nav.view = false;
+      else {
+        this.check.nav.view = true;
       }
     },
-    methods: {
-    },
-    watch: {
-      $route(to) {
-        console.log(to.name)
-        if(this.check.nav.list.includes(to.name))
-          this.check.nav.view = false
-        else {
-          this.check.nav.view = true
-          if(this.check.search.list.includes(to.name)) this.check.search.view = false
-          else this.check.search.view = true
-        }
-      }
-    }
-  }
+  },
+};
 </script>
