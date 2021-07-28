8728f32b638c1e1727817b6ce01bdc1ee57e95ed
jjaekkaemi <21600752@handong.edu>
2021-07-28 11:36:21 +0900

    bandlist ui 작업

--- frontend/src/components/BandList.vue
+++ frontend/src/components/BandList.vue
@@ -1,13 +1,78 @@
 <template>
   <div>
-      BandList
+      <v-list>
+      <v-list-item
+        v-for="chat in recent"
+        :key="chat.title">
+        <v-list-item-icon>
+          <v-icon>mdi-message-outline</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="chat.title"></v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="chat.title"></v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-icon>
+          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
+            mdi-message-outline
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-icon>
+          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
+            mdi-message-outline
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-icon>
+          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'">
+            mdi-message-outline
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-icon>
+          <v-icon :color="chat.active ? 'deep-purple accent-4' : 'grey'" @click="log()">
+            mdi-message-outline
+          </v-icon>
+        </v-list-item-icon>
+
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld';
-
-export default {
-
-};
+  export default {
+    data: () => ({
+      recent: [
+        {
+          active: true,
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Jason',
+        },
+        {
+          active: true,
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Mike',
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Cindy',
+        },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Ali',
+        },
+      ],
+      previous: [{
+        title: 'Travis',
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+      }],
+    }),
+    methods: {
+      log(){
+        console.log("클릭")
+      }
+    }
+  }
 </script>
