cc4195d35a2f207331c9021004ee9ab68ae4a50e
jjaekkaemi <35017054+jjaekkaemi@users.noreply.github.com>
2021-08-19 21:20:51 +0900

    Update appConfig.py

--- backend/server_configuration/appConfig.py
+++ backend/server_configuration/appConfig.py
@@ -11,7 +11,7 @@ class DevelopmentConfig():
 
 class ProductionConfig():
     BIND_PORT = 8082
-    WEB_URL = "https://t-vsm.com"
+    WEB_URL = "http://127.0.0.1"
     SQLALCHEMY_DATABASE_URI = 'mysql://dbadmin:p@ssw0rd@127.0.0.1/efwb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
