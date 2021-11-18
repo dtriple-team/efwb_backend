034d3834ca06b09e09a9bf1d1c0bade4eada0e4f
jjaekkaemi <21600752@handong.edu>
2021-11-18 18:48:54 +0900

    add

--- backend/api/api_band.py
+++ backend/api/api_band.py
@@ -647,7 +647,7 @@ def handle_mqtt_message(client, userdata, message):
         extAddress = hex(int(str(mqtt_data['extAddress']['high'])+str(mqtt_data['extAddress']['low'])))
         mqtt_thread = socketio.start_background_task(handle_sync_data(mqtt_data, extAddress))
       mqtt_thread = None
-    handle_sync_data_(mqtt_data, extAddress)
+    
   elif message.topic == '/efwb/post/connectcheck' :
      handle_gateway_state(json.loads(message.payload))
      #gatewayCheck()
