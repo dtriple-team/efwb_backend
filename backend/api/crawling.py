111ef3d9c578ca4d0e58dc4646faf5f8554b26fb
jjaekkaemi <21600752@handong.edu>
2022-04-15 01:25:36 +0900

    edit weather

--- backend/api/crawling.py
+++ backend/api/crawling.py
@@ -26,6 +26,52 @@ def searchAirpressure(trtemp, atemp, location):
     tdtemp = trtemp[at+2].find_all('td')
     return  float(tdtemp[len(tdtemp)-1].text)
 
+# def getWeather(location):
+#     try:
+#         html = requests.get(
+#             'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query='+location+' 날씨')
+#         soup = BeautifulSoup(html.text, 'html.parser')
+
+#         temp = soup.find("body")
+#         tempor = temp.find("div", {"class":"temperature_text"})
+#         status = temp.find("span", {"class": "weather before_slash"})
+
+#         min = temp.find("span", {"class": "lowest"})
+#         max = temp.find("span", {"class": "highest"})
+#         fore = temp.find_all("li",{"class": "_li"})
+        
+#         wind = temp.find_all("dt", {"class": "term"})
+#         wind_strength = temp.find_all("dd", {"class": "desc"})
+
+#         forecast = []
+#         for fo in range(4):
+#             if fore[fo*2].find("dt", {"class": "time"}).text == "내일":
+#                 forecast.append({"time":"00시", "value":fore[fo*2].find("span", {"class": "num"}).text})
+#             else :
+#                 forecast.append({"time":fore[fo*2].find("dt", {"class": "time"}).text, "value":fore[fo*2].find("span", {"class": "num"}).text})
+
+#         html = requests.get(
+#             'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query='+location+" 미세먼지")
+#         soup = BeautifulSoup(html.text, 'html.parser')
+
+#         temp = soup.find("div", {"class": "air_nextday_city"})
+#         temp = temp.find_all("dd", {"class": "lvl"})
+        
+#         result = {
+#             "temp": tempor.text.replace(" 현재 온도", "").replace(" ", ""),
+#             "status": status.text,
+#             "min":min.text.replace("최저기온", ""),
+#             "max":max.text.replace("최고기온", ""),
+#             "finedust": temp[0].text,
+#             "ultrafinedust":temp[0].text,
+#             "forecast": forecast,
+#             "wind":wind[2].text,
+#             "wind_strength":wind_strength[2].text
+#         }
+#         return result
+#     except:
+#         result={"temp": 0}
+#         return result
 def getWeather(location):
     try:
         html = requests.get(
@@ -33,40 +79,16 @@ def getWeather(location):
         soup = BeautifulSoup(html.text, 'html.parser')
 
         temp = soup.find("body")
-        tempor = temp.find("div", {"class":"temperature_text"})
-        status = temp.find("span", {"class": "weather before_slash"})
+        status = temp.find("p", {"class":"cast_txt"})
+        tempor = temp.find("span", {"class":"todaytemp"})
+        wind = temp.find_all("span", {"class": "num"})
 
-        min = temp.find("span", {"class": "lowest"})
-        max = temp.find("span", {"class": "highest"})
-        fore = temp.find_all("li",{"class": "_li"})
-        
-        wind = temp.find_all("dt", {"class": "term"})
-        wind_strength = temp.find_all("dd", {"class": "desc"})
-
-        forecast = []
-        for fo in range(4):
-            if fore[fo*2].find("dt", {"class": "time"}).text == "내일":
-                forecast.append({"time":"00시", "value":fore[fo*2].find("span", {"class": "num"}).text})
-            else :
-                forecast.append({"time":fore[fo*2].find("dt", {"class": "time"}).text, "value":fore[fo*2].find("span", {"class": "num"}).text})
-
-        html = requests.get(
-            'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query='+location+" 미세먼지")
-        soup = BeautifulSoup(html.text, 'html.parser')
-
-        temp = soup.find("div", {"class": "air_nextday_city"})
-        temp = temp.find_all("dd", {"class": "lvl"})
-        
         result = {
-            "temp": tempor.text.replace(" 현재 온도", "").replace(" ", ""),
-            "status": status.text,
-            "min":min.text.replace("최저기온", ""),
-            "max":max.text.replace("최고기온", ""),
-            "finedust": temp[0].text,
-            "ultrafinedust":temp[0].text,
-            "forecast": forecast,
-            "wind":wind[2].text,
-            "wind_strength":wind_strength[2].text
+            "temp": str(tempor.text)+"℃",
+            "status": str(status.text).split(", ")[0],
+            "humidity":wind[1].text+"%",
+            "uv":wind[2].text,
+            "wind":wind[0].text+"m/s",
         }
         return result
     except:
