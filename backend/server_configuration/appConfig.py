27cc015e8d5463bc37863f0d4c383e8e1090aa94
jjaekkaemi <21600752@handong.edu>
2023-05-09 15:52:01 +0900

    edit

--- backend/server_configuration/appConfig.py
+++ backend/server_configuration/appConfig.py
@@ -4,7 +4,6 @@
 
 class DevelopmentConfig():
     BIND_PORT = 8081
-    WEB_URL = "http://127.0.0.1"
     SQLALCHEMY_DATABASE_URI = 'mysql://root:p@ssw0rd@127.0.0.1/efwb2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MQTT_BROKER_URL = "mj.d-triple.com"
@@ -13,7 +12,6 @@ class DevelopmentConfig():
 
 class ProductionConfig():
     BIND_PORT = 8081
-    WEB_URL = "http://210.220.151.77/"
     SQLALCHEMY_DATABASE_URI = 'mysql://dbadmin:p@ssw0rd@127.0.0.1/efwb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MQTT_BROKER_URL = "mj.d-triple.com"
