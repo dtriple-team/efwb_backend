6a9d14f0826f914376af6a1b517d5785ed397f28
Hanul <hanul.devleoper@gmail.com>
2025-01-09 13:48:42 +0900

    Fix: import uuid, dev server

--- backend/server_configuration/appConfig.py
+++ backend/server_configuration/appConfig.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import uuid
 
 class DevelopmentConfig():
     BIND_PORT = 8080
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:p@ssw0rd@127.0.0.1/efwb2'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@127.0.0.1:3307/efwb3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MQTT_BROKER_URL = "3.37.65.94"
     MQTT_BROKER_PORT = 18831
