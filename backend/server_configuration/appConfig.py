33e5be823bea3c68f103fe22d0b2d432d2b38b8d
phb1018 <33706406+parkhongbeom@users.noreply.github.com>
2025-05-14 11:10:01 +0900

    DB 데이터 베이스 변경(efwb3 -> naas)

--- backend/server_configuration/appConfig.py
+++ backend/server_configuration/appConfig.py
@@ -4,7 +4,7 @@ import uuid
 
 class DevelopmentConfig():
     BIND_PORT = 8080
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@127.0.0.1:3307/efwb3'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@127.0.0.1:3307/naas'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MQTT_BROKER_URL = "3.37.65.94"
     MQTT_BROKER_PORT = 18831
@@ -15,7 +15,7 @@ from urllib.parse import quote_plus
 password = quote_plus('p@ssw0rd')
 class ProductionConfig():
     BIND_PORT = 5000
-    SQLALCHEMY_DATABASE_URI = f'mysql://dbadmin:{password}@127.0.0.1/efwb3'
+    SQLALCHEMY_DATABASE_URI = f'mysql://dbadmin:{password}@127.0.0.1/naas'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MQTT_BROKER_URL = '3.37.65.94'
     MQTT_BROKER_PORT = 18831
